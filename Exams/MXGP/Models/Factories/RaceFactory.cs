cdba6921f6c6e3415a4c857efa87c0069ceffa62
Atanas Kolev <naskokolev00@gmail.com>
2019-08-09 13:04:47 +0300

    fixing bugs

---
+++ Exams/MXGP/Models/Factories/RaceFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace MXGP.Models.Factories
+{
+    using MXGP.Models.Factories.Contracts;
+    using MXGP.Models.Races;
+    using MXGP.Models.Races.Contracts;
+
+    public class RaceFactory : IRaceFactory
+    {
+        public Race Create(string name, int laps)
+        {
+            return new Race(name, laps);
+        }
+    }
+}
