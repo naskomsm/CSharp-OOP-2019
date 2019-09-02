f7e7998c60da252936810165f7d3d7841b0ba669
Atanas Kolev <naskokolev00@gmail.com>
2019-09-02 14:11:44 +0300

    added repos

---
+++ Exams/ViceCity/Repositories/PlayerRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace ViceCity.Repositories
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using ViceCity.Models.Players;
+    using ViceCity.Models.Players.Contracts;
+
+    public class PlayerRepository
+    {
+        private List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+
+            var mainPlayer = new MainPlayer("Vercetti");
+            this.players.Add(mainPlayer);
+        }
+
+        public int Count => this.players.Count - 1;
+
+        public string Add(string name)
+        {
+            var player = new CivilPlayer(name);
+            this.players.Add(player);
+
+            return $"Successfully added civil player: {player.Name}!";
+        }
+
+        public IPlayer GetByName(string name)
+        {
+            return this.players.FirstOrDefault(x => x.Name == name);
+        }
+
+        public List<IPlayer> CivilPlayers()
+        {
+            return this.players.Where(x => x.GetType().Name == "CivilPlayer").ToList();
+        }
+    }
+}
